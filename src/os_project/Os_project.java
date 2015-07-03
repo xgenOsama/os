1cd940b60722949c216e4b50c87935b0e9632218
xgenOsama <xgen.osama@gmail.com>
2015-07-03 09:01:48 +0200

    first commit

---
+++ src/os_project/Os_project.java
@@ -0,0 +1,16 @@
+package os_project;
+
+/**
+ *
+ * @author genosama
+ */
+public class Os_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LogFrame().setVisible(true);
+    }
+    
+}
